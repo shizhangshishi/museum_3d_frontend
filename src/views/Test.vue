ce2e6a6320b7310540bf098469d4486508a4293e
Luo Hongliang <358024958@qq.com>
2021-05-20 19:27:47 +0800

    test room

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,52 +1,25 @@
 <template>
-  <div>
-    <div id="container"></div>
-  </div>
+  <Room :roomId = roomId></Room>
 </template>
-
 <script>
-import * as Three from 'three'
 
+import Room from "@/components/museum/Room";
 export default {
-  name: 'Test',
-  data () {
+  name: 'CustomerThree',
+  components: {Room},
+  label:'3D模型',
+  data() {
     return {
-      camera: null,
-      scene: null,
-      renderer: null,
-      mesh: null
+      roomId: 1
     }
   },
   methods: {
-    init: function () {
-      let container = document.getElementById('container')
-      this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10)
-      this.camera.position.z = 0.6
-      this.scene = new Three.Scene()
-      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
-      let material = new Three.MeshNormalMaterial()
-      this.mesh = new Three.Mesh(geometry, material)
-      this.scene.add(this.mesh)
 
-      this.renderer = new Three.WebGLRenderer({antialias: true})
-      this.renderer.setSize(container.clientWidth, container.clientHeight)
-      container.appendChild(this.renderer.domElement)
-    },
-    animate: function () {
-      requestAnimationFrame(this.animate)
-      this.mesh.rotation.x += 0.01
-      this.mesh.rotation.y += 0.02
-      this.renderer.render(this.scene, this.camera)
-    }
   },
-  mounted () {
-    this.init()
-    this.animate()
+  mounted() {
+
   }
 }
 </script>
 <style scoped>
-#container {
-  height: 400px;
-}
 </style>
