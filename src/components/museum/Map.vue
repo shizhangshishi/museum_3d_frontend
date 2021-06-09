9dc9ec9b67973d77b12b8b99ce336131217bf1e5
Honminden <honminden@outlook.com>
2021-06-10 03:47:37 +0800

    修改摄像机视角操作，增加地图提示

--- src/components/museum/Map.vue
+++ src/components/museum/Map.vue
@@ -8,6 +8,24 @@
         <v-btn @click="close">关闭</v-btn>
       </v-col>
     </v-card-title>
+    <v-card-text>
+      <h2>操作提示</h2>
+      <p>按WASD或上下左右键进行移动</p>
+      <p>滚动鼠标滚轮进行视角缩放</p>
+      <p>按住鼠标中键并移动鼠标进行视角旋转</p>
+      <hr/>
+      <h2>展区位置一览</h2>
+      <v-row>
+        <v-col class="col-4">核废水展区</v-col>
+        <v-col class="col-4">切尔诺贝利事件展区</v-col>
+        <v-col class="col-4">福岛事件展区</v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col-4"></v-col>
+        <v-col class="col-4">大厅</v-col>
+        <v-col class="col-4"></v-col>
+      </v-row>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -32,5 +50,9 @@ export default {
   left: 20%;
   top: 20%;
   background: rgba(256, 256, 256, 0.9);
+}
+
+h2{
+  margin-bottom: 1em;
 }
 </style>
