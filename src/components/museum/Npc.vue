c0edb7bda5797613fe34e533a236b49eef258ab6
Honminden <honminden@outlook.com>
2021-06-23 00:20:48 +0800

    加入接待员台词

--- src/components/museum/Npc.vue
+++ src/components/museum/Npc.vue
@@ -13,7 +13,10 @@
         <v-card-title>
           知识问答
         </v-card-title>
-        <v-card-text>
+        <v-card-text id="content">
+          <v-row>
+            欢迎来到核泄漏博物馆，如果已经学习到了核泄漏相关的知识，可以来试试答题哦。
+          </v-row>
           <v-row>
             <v-btn text @click="question(1)" color="primary" width="100%">核科学知识展区</v-btn>
           </v-row>
@@ -53,5 +56,9 @@ export default {
   left: 20%;
   top: 20%;
   background: rgba(256, 256, 256, 0.9);
+}
+
+#content{
+  padding: 5%;
 }
 </style>
